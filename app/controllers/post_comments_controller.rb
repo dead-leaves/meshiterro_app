75ddbb5e7fcf9edb6e9586bf0fde41a63328254e
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2022-09-10 15:45:38 +0000

    meshiterro10day

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -9,6 +9,11 @@ class PostCommentsController < ApplicationController
   end
   
   
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
+  end
+  
   
    private
   
