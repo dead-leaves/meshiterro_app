f71211e2ad1d6ee17b59d725f16fa491709bd19f
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2022-09-12 14:44:16 +0000

    meshiterro11day

--- app/controllers/ravorites_controller.rb
+++
@@ -1,2 +0,0 @@
-class RavoritesController < ApplicationController
-end
--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class RavoritesControllerTest < ActionDispatch::IntegrationTest
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
